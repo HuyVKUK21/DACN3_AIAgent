3a2bc00923202f58983bfbc16d0a9aa3e1260792
HuyVKUK21 <pqhuy2510@gmail.com>
2025-04-12 03:00:00 +0000

    Config VNPay Payment

---
+++ src/main/java/com/fsoft/config/VNPayConfig.java
@@ -0,0 +1,8 @@
+package com.fsoft.config;
+
+public class VNPayConfig {
+    public static final String vnp_TmnCode = "YYJ679TA"; 
+    public static final String vnp_HashSecret = "CKDHO1HXRBXKTDQTXFYKL6RK4GI9VAI9"; 
+    public static final String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+    public static final String vnp_ReturnUrl = "http://localhost:8080/return-vnpay"; 
+}
